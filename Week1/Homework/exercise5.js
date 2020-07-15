a40a1c96782456a72aec6b444228c384ec7bea93
HackYourFuture <hackyourfuture@hackyouturesair.home>
2020-07-15 14:23:52 +0200

    Finished Exercise 5: Round a number and log it

---
+++ Week1/Homework/exercise5.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Exercise 5: Round a number and log it.
+
+let z = 7.25;
+console.log(z);
+let a = Math.round(z);
+console.log(a);
+
+let highestValue;
+
+if (a > z) {
+    console.log(highestValue = a);
+  } else {
+      console.log(highestValue = z);
+  }
