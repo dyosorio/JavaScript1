3eee8916e8e40bfa0d0a22e35ace60d141045d19
HackYourFuture <hackyourfuture@hackyouturesair.home>
2020-07-15 14:10:04 +0200

    Finished Exercise 4: Log the string

---
+++ Week1/Homework/exercise4.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Exercise 4: Log the string;
+
+let myString = "Danny Osorio Chavarria";
+console.log("the value of the string should be my full name");
+console.log(myString);
+myString = "Valentina Urroz Osorio";
+console.log("the value of myString should be my daughter's name");
+console.log(myString);
