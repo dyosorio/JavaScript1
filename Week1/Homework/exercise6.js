434e643ffd1e170c1ac4fe98c29b30f8d0d715d4
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2020-07-15 23:24:24 +0200

    Finished Exercise 6: Log an array of animals

---
+++ Week1/Homework/exercise6.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Exercise 6: Log an array of animals
+
+let items = [];
+console.log("value of the array is a list of zero elements");
+console.log(items);
+console.log(items.length > 0);
+let myFavoriteAnimals = ['cats', 'cows', 'horses'];
+console.log(myFavoriteAnimals);
+myFavoriteAnimals.push('Piglet');
+console.log(myFavoriteAnimals);
+console.log(myFavoriteAnimals.length > 0);
