af6c8f7bcf980de05ad7b1abdd4929427d65c3ab
HackYourFuture <hackyourfuture@hackyouturesair.home>
2020-07-15 13:25:19 +0200

    Finished Exercise 1: Hello world

---
+++ Week1/Homework/exercise1.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Exercise 1: Hello world!
+
+console.log("Hello World!");
+console.log("Hola Mundo!");
+console.log("Hello Welt!");
+console.log("Salve Orbis Terrarum!");
+console.log("Ciao Mondo!");
+console.log("Witaj świecie!");
+console.log("Hey Verden!");
+console.log("Hola món!");
+console.log("Hei Verden!");
+console.log("Hey världen!");
+
+
