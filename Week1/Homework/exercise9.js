6dd5d9ef716aef52ef068d47577f4401098bb282
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2020-07-16 12:24:45 +0200

    Finished Exercise 9: Log the remainder

---
+++ Week1/Homework/exercise9.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Exercise 9: Log the remainder
+
+let x = 7; 
+/*the result of the operation below is 1; because the number 3 fits two times within the number 7 with a remaining of 1*/
+console.log(x = x % 3); 
+
+/*the result of the operation below is 1; because the number 4 fits five times within the number 21 with a remaining of 1*/
+let y = 21;
+console.log(y = y % 4); 
+
+/*the result of the operation below is 1; because the number 2 fits six times within the number 13 with a remaining of 1*/
+let z = 13;
+console.log(z = z % 2); 
+
+/*the result of the operation below is 3; because the number 5 fits 24 times within the number 123 with a remaining of 3*/
+let w = 123;
+console.log(w = w % 5); 
