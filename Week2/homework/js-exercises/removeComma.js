31b8fd4c66de4b9d203bd00c0d19d5339b5822a3
HackYourFuture <hackyourfuture@HackYouturesAir.fritz.box>
2020-07-23 11:28:37 +0200

    Finished Exercise 1: Remove the comma

---
+++ Week2/homework/js-exercises/removeComma.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// Exercise 1: Remove the comma
+
+
+let myString = 'hello,this,is,a,difficult,to,read,sentence';
+console.log(myString.length);
+
+myString = myString.replace(/,/g, ' ');
+
+console.log(myString);
+
+
