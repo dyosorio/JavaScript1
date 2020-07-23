6e6e9a1f92a63858d58a9ec069643eded1de6bc1
HackYourFuture <hackyourfuture@HackYouturesAir.fritz.box>
2020-07-23 22:36:23 +0200

    Finished Exercise 2: The even/odd reporter

---
+++ Week2/homework/js-exercises/oddReporter.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Exercise 2: The even/odd reporter
+
+for (let i = 0; i < 21; i++) {
+    // more statements
+     if((i % 2) !== 0){
+     console.log('The number ' + i + ' is odd!');
+     } else {
+         console.log('The number ' + i + ' is even!') 
+     }
+ }
