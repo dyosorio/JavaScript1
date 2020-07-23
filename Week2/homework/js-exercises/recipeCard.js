8003a9a4ef712f891f18c87acd7ef102cda6c97e
HackYourFuture <hackyourfuture@HackYouturesAir.fritz.box>
2020-07-23 23:36:40 +0200

    Finished //Exercise 3: The recipe card

--- Week2/homework/js-exercises/recipeCard.js
+++ Week2/homework/js-exercises/recipeCard.js
@@ -1,3 +1,17 @@
+//Exercise 3: The recipe card
+
 'use strict';
 
-//Exercise 3: The recipe card
+const myMeal = { 
+	Name: 'Spare Ribs', 
+	Serves: 4, 
+	Ingredients: ['Pork ribs', 'Secret ingredient', 'BBQ sauce'] 
+};
+
+//for in...loop, because the other loops didn't show the property name only the property value.
+for (const property in myMeal) {
+  console.log(`${property}: ${myMeal[property]}`);
+}
+
+
+// source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
