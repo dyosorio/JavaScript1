071a3d100e3d68f20fd258f0d0b6726218d332f0
HackYourFuture <hackyourfuture@HackYouturesAir.fritz.box>
2020-07-25 12:16:29 +0200

    Finished // Project: grade calculator

--- Week2/homework/js-exercises/projectGradeCalculator.js
+++ Week2/homework/js-exercises/projectGradeCalculator.js
@@ -1,74 +1,85 @@
 'use strict';
 
-/*4. PROJECT: Grade calculator
+//4. PROJECT: Grade calculator
 
-In this project you'll write a function that calculates grades. Let's say a student did a test and they got a 60 out of 100, this function will:
+//convert the score into a percentage - done!
+//calculate what grade corresponds with that percentage - done!
 
-convert the score into a percentage - done
-calculate what grade corresponds with that percentage, and
-shows in the command line the result: the grade and the percentage done
+//In this example this is what we would expect the function to return in the command line:
+//You got a D (60%)!
+//When writing the function, make use of the following grade scores:
+//Grade A (90% - 100%)
+//Grade B (80% - 89%)
+//Grade C (70% - 79%)
+//Grade D (60% - 69%)
+//Grade E (50% - 59%)
+//Grade F (0% - 49%)
+//These are the requirements your project needs to fulfill:
 
-In this example this is what we would expect the function to return in the command line:
-You got a D (60%)!
-When writing the function, make use of the following grade scores:
+//Make a JavaScript file - done!
+//Use either a switch or if/else statement - done!
+//Write at least 2 comments - done!
+//Make the return value of the function a template string - done!
+//Use node from the command line to test if your code works as expected - done!
 
-Grade A (90% - 100%)
-Grade B (80% - 89%)
-Grade C (70% - 79%)
-Grade D (60% - 69%)
-Grade E (50% - 59%)
-Grade F (0% - 49%)
-These are the requirements your project needs to fulfill:
 
-Make a JavaScript file with a name that describes its contents - done
-Use either a switch or if/else statement
-Write at least 2 comments that explain to others what a line of code is meant to do
-Make the return value of the function a template string, so you can insert variables!
-Use node from the command line to test if your code works as expected
-Good luck!*/
-
-//write a function that converts the score into a percentage
+//First step: write a function that converts the score into a percentage
 
 function gradeCalc(grade) {
-    //LocaleString 
+    //Second step: convert the score into percentage. The toLocaleString() method converts the score to a percentage representation of the number, and then returns its value as a string.
       var num= grade;
-      var s = Number(num/100).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:0}); 
-      //return s;
+      var s = Number(num/100).toLocaleString(undefined,{style: 'percent', maximumFractionDigits:0}); 
+      //return s; make sure var s is returning the score as a percentage. 
+
+      var a = 'A';
+      var b = 'B';
+      var c = 'C';
+      var d = 'D';
+      var e = 'E';
+      var f = 'F';
   
-      if (grade < 100 && grade > 90) {
-          return `You got and A (${s})!`
-      } if (grade < 89 && grade > 80) {
-          return `You got a B (${s})!`
-      } if (grade < 79 && grade > 70) {
-          return `You got a C (${s})!`
-      }if (grade < 69 && grade > 60) {
-          return `You got a D (${s})!`
-      }if (grade < 59 && grade > 50) {
-          return `You got an E (${s})!`
+      //Third step: calculate what grades correspond to what percentage.
+      if (grade <= 100 && grade >= 90) {
+          return `You got and ${a} (${s})!` //Fourth step: used Template Literal to use var in a string
+      } if (grade <= 89 && grade >= 80) {
+          return `You got a ${b} (${s})!`
+      } if (grade <= 79 && grade >= 70) {
+          return `You got a ${c} (${s})!`
+      }if (grade <= 69 && grade >= 60) {
+          return `You got a ${d} (${s})!`
+      }if (grade <= 59 && grade >= 50) {
+          return `You got an ${e} (${s})!`
       } else {
-          return `You got an F (${s})!`
+          return `You got an ${f} (${s})!`
       }
+
+
       
+      // I tried to use a switch statement instead but the template literal didn't work. The percentage is correct but all the cases are printing F by default.
+      /* switch (grade) {
+        case grade <= 100 && grade >= 90:
+          return `You got and ${a} (${s})!`
+          break;
+        case grade <= 89 && grade >= 80:
+          return `You got a ${b} (${s})!`
+          break;
+        case grade <= 79 && grade >= 70:
+          return `You got a ${c} (${s})!`
+          break;
+        case grade <= 69 && grade >= 60:
+          return `You got a ${d} (${s})!`
+          break;
+        case grade <= 59 && grade >= 50:
+          return `You got an ${e} (${s})!`
+          break;
+        default:
+          return `You got an ${f} (${s})!`
+          break;
+        }*/
+        
     }
   
-  console.log(gradeCalc(92));
+  console.log(gradeCalc(100));
   
-  /*witch (expression) {
-      case value1:
-        //Statements executed when the
-        //result of expression matches value1
-        [break;]
-      case value2:
-        //Statements executed when the
-        //result of expression matches value2
-        [break;]
-      ...
-      case valueN:
-        //Statements executed when the
-        //result of expression matches valueN
-        [break;]
-      [default:
-        //Statements executed when none of
-        //the values match the value of the expression
-        [break;]]
-    }*/
+
+   
