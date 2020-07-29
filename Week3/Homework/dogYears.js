febdde23c31537a3de50affb6c60ad2322351e80
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2020-07-29 11:28:10 +0200

    Revised and Finished //Exercise 2: Dog years

---
+++ Week3/Homework/dogYears.js
@@ -0,0 +1,20 @@
+'use strict';
+
+//Exercise 2: Dog years
+
+//Write a function named calculateDogAge.
+//It takes 1 argument: your (fictional) puppy's age (number).
+//Calculate your dog's age based on the conversion rate of 1 human year to 7 dog years.
+//Return a string: "Your doggie is [CALCULATED_VALUE] years old in dog years!"
+
+
+function calculateDogAge(age) {
+    let dogAge = age * 7;
+
+    return `Your doggie is ${dogAge} years old in dog years!`
+
+}
+
+console.log(calculateDogAge(1));
+console.log(calculateDogAge(2));
+console.log(calculateDogAge(3));
