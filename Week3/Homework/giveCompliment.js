136a4e229a7f0718d328642fd68d304a41cf356b
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2020-07-29 10:25:57 +0200

    added the return keyword inside the function and console.log to call the function outside the function body

--- Week3/Homework/giveCompliment.js
+++ Week3/Homework/giveCompliment.js
@@ -2,12 +2,11 @@
 
 //Exercise 1: You are amazing, Danny!
 
-/*Write a function named giveCompliment - done
+/*Write a function named giveCompliment.
 It takes 1 argument: your name - done
-Inside the function define a variable that holds an array, compliments, with 10 strings. Each string should be a compliment, like "great", "awesome"
+Inside the function define a variable that holds an array, compliments, with 10 strings. Each string should be a compliment.
 Write code that randomly selects a compliment
-Return a string: "You are [COMPLIMENT], [YOUR_NAME]!
-Call the function three times, giving each function call the same argument: your name.*/
+Return a string: "You are [COMPLIMENT], [YOUR_NAME]!*/
 
 function giveCompliment(name) {
 
@@ -29,12 +28,12 @@ function giveCompliment(name) {
     // Math.floor() returns the largest number
     // Math.random() returns a number in the range of 0 to less than 1
 
-    console.log(`You are ${randomCompliment}, ${name}!`)
+    return `You are ${randomCompliment}, ${name}!`;
 }
 
 
-giveCompliment('Danny');
+console.log(giveCompliment('Danny'));
 
-//Math.floor(Math.random()*compliments.length)
+
 // source 1: https://www.kirupa.com/html5/picking_random_item_from_array.htm
 // source 2: https://www.kirupa.com/html5/random_numbers_js.htm
